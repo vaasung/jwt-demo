fad2ce7beb07f4cb9ff10119d038055e7b34dcb6
“Vasu” <vasuthevan.p@cogentibs.com>
2022-11-13 08:50:52 +0530

    view ejs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(express.static('public'))
 app.use([express.json(), express.urlencoded({ extended: true })])
+app.set('views', './views');
 app.set('view engine', 'ejs')
 
 app.use(router)
