6f099bfefc27974ef2a38bf88d0b5008514d5434
“Vasu” <vasuthevan.p@cogentibs.com>
2022-11-13 09:03:18 +0530

    path added

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const router = require('./routes/authRouter')
 
 const { PORT } = process.env
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use([express.json(), express.urlencoded({ extended: true })])
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(router)
