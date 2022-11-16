dfdb1c484987ec9fb87a3b76c830c839aeaeeb65
“Vasu” <vasuthevan.p@cogentibs.com>
2022-11-17 00:27:19 +0530

    404 page added

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const movieRouter = require('./routes/movieRouter')
 const { PORT, WHITE_LISTING_URLs } = process.env
 const app = express()
 
-// app.use('/public', express.static('public'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use([express.json(), express.urlencoded({ extended: true })])
 app.use(cookieParser())
@@ -42,7 +41,9 @@ app.get('/', (req, res) => res.render('pages/home', { token: req.cookies?.token 
 app.get('/add', (req, res) => res.render('pages/add'))
 app.get('/edit', (req, res) => res.render('pages/edit'))
 app.get('/view', (req, res) => res.render('pages/view'))
-app.get('/register', (req, res) => res.render('pages/register'))
-app.get('/login', (req, res) => res.render('pages/login'))
+app.get('/register', (req, res) => res.render('pages/register', { token: req.cookies?.token }))
+app.get('/login', (req, res) => res.render('pages/login', { token: req.cookies?.token }))
+app.get('/unAuth', (req, res) => res.render('pages/unAuth'))
+app.get('/*', (req, res) => res.render('pages/404'))
 
 app.listen(PORT || 4000, console.log(`App started running ${PORT}`))
